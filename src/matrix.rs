00561dbef3dc81814984ae2a8e859ed71858c27e
Olle Wreede <olle@wreede.se>
2024-10-07 23:56:03 +0200

    Improve matrix indexing

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use crate::float::*;
 
 pub fn matrix(t1: MatrixRow, t2: MatrixRow, t3: MatrixRow, t4: MatrixRow) -> Matrix {
@@ -5,20 +7,25 @@ pub fn matrix(t1: MatrixRow, t2: MatrixRow, t3: MatrixRow, t4: MatrixRow) -> Mat
 }
 
 pub type MatrixRow = [Float; 4];
+pub type MatrixIndex = (usize, usize);
 pub struct Matrix([MatrixRow; 4]);
 
 impl Matrix {
     pub fn new(t1: MatrixRow, t2: MatrixRow, t3: MatrixRow, t4: MatrixRow) -> Self {
         Matrix([t1, t2, t3, t4])
     }
+}
 
-    pub fn get(&self, row: usize, col: usize) -> Float {
-        self.0[row][col]
+impl Index<MatrixIndex> for Matrix {
+    type Output = Float;
+
+    fn index(&self, index: MatrixIndex) -> &Self::Output {
+        &self.0[index.0][index.1]
     }
 }
 
 #[cfg(test)]
-mod test_chapter_1_maths {
+mod test_chapter_3_matrices {
     use super::*;
 
     #[test]
@@ -29,12 +36,12 @@ mod test_chapter_1_maths {
             [9.0, 10.0, 11.0, 12.0],
             [13.5, 14.5, 15.5, 16.5],
         );
-        assert_eq!(m.get(0, 0), 1.0);
-        assert_eq!(m.get(0, 3), 4.0);
-        assert_eq!(m.get(1, 0), 5.5);
-        assert_eq!(m.get(1, 2), 7.5);
-        assert_eq!(m.get(2, 2), 11.0);
-        assert_eq!(m.get(3, 0), 13.5);
-        assert_eq!(m.get(3, 2), 15.5);
+        assert_eq!(m[(0, 0)], 1.0);
+        assert_eq!(m[(0, 3)], 4.0);
+        assert_eq!(m[(1, 0)], 5.5);
+        assert_eq!(m[(1, 2)], 7.5);
+        assert_eq!(m[(2, 2)], 11.0);
+        assert_eq!(m[(3, 0)], 13.5);
+        assert_eq!(m[(3, 2)], 15.5);
     }
 }
